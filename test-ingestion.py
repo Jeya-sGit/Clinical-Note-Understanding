48d75c80303c3c7378d982e901e3b2dd1845a193
JEYA NAGARAJA J <jjeyanagaraja8602@gmail.com>
2026-04-20 12:19:45 +0530

    test: add ingestion testing for PDF extraction and chunking

---
+++ test-ingestion.py
@@ -0,0 +1,12 @@
+from ingestion.ingestion import process_multiple_pdfs
+
+pdf_files = ["Clinical Doc 1.pdf", "Clinical Doc 2.pdf", "Clinical Doc 3.pdf"]
+
+
+chunks = process_multiple_pdfs(pdf_files)
+
+# Print sample output
+for i, chunk in enumerate(chunks[:5]):
+    print(f"Chunk {i+1}:")
+    print(chunk)
+    print("----------")
